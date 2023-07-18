9fe99f7292a44b0e70735c102193e6d0845d5b6d
Priya-rgb-arch <67141434+Priya-rgb-arch@users.noreply.github.com>
2023-07-18 12:42:37 +0530

    Update SkopeRejectAlert.sh

--- SkopeAlertUploader/script/SkopeRejectAlert.sh
+++ SkopeAlertUploader/script/SkopeRejectAlert.sh
@@ -6,11 +6,13 @@ ALERT_EMAIL="reshmachavan12196@gmail.com"
 # Set the threshold for the number of rejects to trigger an alert
 REJECTS_THRESHOLD=10
 # Check if the SKOPE rejects file exists
-if [ -f "$SKOPE_REJECTS_FILE" ]; then
+if [ -f "$SKOPE_REJECTS_FILE" ];
+then
   # Get the number of rejects
   num_rejects=$(grep -c '^reject' "$SKOPE_REJECTS_FILE")
   # Compare the number of rejects with the threshold
-  if [ "$num_rejects" -ge "$REJECTS_THRESHOLD" ]; then
+  if [ "$num_rejects" -ge "$REJECTS_THRESHOLD" ];
+  then
     # Send an email alert
     echo "Alert: SKOPE rejects exceeded the threshold of $REJECTS_THRESHOLD" | mail -s "SKOPE Rejects Alert" "$ALERT_EMAIL"
   fi
